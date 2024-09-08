616adfb1c0c8f79d5e02b7f0c53240596c3a38c6
Jorge Garcia <61385320+JcGarciaMed@users.noreply.github.com>
2024-09-08 10:10:00 -0500

    adding h2 db config properties

--- src/main/java/com/greymatter/loans/LoansApplication.java
+++ src/main/java/com/greymatter/loans/LoansApplication.java
@@ -1,9 +1,36 @@
 package com.greymatter.loans;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Loans microservice REST API Documentation",
+                description = "Greymatter bank Loans microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Jorge Garcia",
+                        email = "jcgarciamed@gmail.com",
+                        url = "https://www.greymatter.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.greymatter.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Greymatter bank Loans microservice REST API Documentation",
+                url = "https://www.greymatter.com/swagger-ui.html"
+        )
+)
 public class LoansApplication {
 
     public static void main(String[] args) {
